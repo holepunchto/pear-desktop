7f89d38cbfe03a48aa89a1428ffd2fca99084364
dmc <david.mark.clements@gmail.com>
2025-02-18 16:43:54 +0100

    client-side routing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Bridge from 'pear-bridge'
 
 const runtime = new Runtime()
 
-const bridge = new Bridge()
+const bridge = new Bridge({ waypoint: '/index.html' })
 await bridge.ready()
 
 const pipe = await runtime.start({ bridge })
