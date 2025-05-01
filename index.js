3721a18023729b6a576c319de0bdd2303774ee17
Andrei Regiani <andrei.cpp@gmail.com>
2025-05-01 18:28:20 +0700

    remove Pear.teardown (#33)

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 import Runtime from 'pear-electron'
 import Bridge from 'pear-bridge'
 
-const runtime = new Runtime()
-
-const bridge = new Bridge({ waypoint: '/index.html' })
+const bridge = new Bridge()
 await bridge.ready()
 
-const pipe = await runtime.start({ bridge })
-Pear.teardown(() => pipe.end())
+const runtime = new Runtime()
+await runtime.start({ bridge })
