4464031060a58c404ae119b5fecb4a7662684053
Geordan Gesink <georgegeordan@gmail.com>
2025-07-04 14:41:25 +0200

    improve comprehensability

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Bridge from 'pear-bridge'
 
 const bridge = new Bridge()
 await bridge.ready()
-console.log('hello')
+
 const runtime = new Runtime()
 const pipe = await runtime.start({ bridge })
 
