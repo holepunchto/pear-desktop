eb3cf302d6daa9616ae1eaf948f4344e37751927
Mikkel Malmberg <mikkel@brnbw.com>
2026-04-30 13:05:05 +0200

    Allow git in CI

--- vendor/pear-runtime-electron/src/preload.js
+++
@@ -1,6 +0,0 @@
-import preload from './preload.cjs'
-
-export const createChannels = preload.createChannels
-export const normalizeSpecifier = preload.normalizeSpecifier
-export const toBuffer = preload.toBuffer
-export const createPreloadBridge = preload.createPreloadBridge
